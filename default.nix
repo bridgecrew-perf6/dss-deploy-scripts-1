614428b804125468a688998bbb2657a1d71143d7
jaeik-kim <jaeik1.kim@gmail.com>
2022-02-25 13:43:27 +0800

    init xsc deploy branch

--- default.nix
+++ default.nix
@@ -34,25 +34,21 @@ let
     (optinalFunc (! isNull githubAuthToken) recAddGithubToken)
       (spec // {
         inherit doCheck;
-        solc = solc-versions.solc_0_5_12;
+        solc = solc-versions.solc_0_6_12;
       })
   ) deps);
 
   # Create derivations from lock file data
   packages = packageSpecs (deps' // {
     # Package overrides
-    clipper-mom = deps'.clipper-mom                 // { name = "clipper-mom-optimized";       solcFlags = "--optimize --optimize-runs 200"; solc = solc-static-versions.solc_0_6_12; };
-    ilk-registry = deps'.ilk-registry               // { name = "ilk-registry-optimized";      solcFlags = "--optimize --optimize-runs 1000000"; solc = solc-static-versions.solc_0_6_12; };
-    dss-auto-line = deps'.dss-auto-line             // { name = "dss-auto-line-optimized";     solcFlags = "--optimize --optimize-runs 1000000"; solc = solc-static-versions.solc_0_6_11; };
-    dss-flash = deps'.dss-flash                     // { name = "dss-flash";                   solc = solc-static-versions.solc_0_6_12; };
+    dss-clipper-mom = deps'.dss-clipper-mom         // { name = "dss-clipper-mom-optimized";   solcFlags = "--optimize --optimize-runs 200"; };
+    dss-ilk-registry = deps'.dss-ilk-registry       // { name = "dss-ilk-registry-optimized";  solcFlags = "--optimize --optimize-runs 1000000"; };
+    dss-auto-line = deps'.dss-auto-line             // { name = "dss-auto-line-optimized";     solcFlags = "--optimize --optimize-runs 1000000"; };        
     dss-proxy-actions = deps'.dss-proxy-actions     // { name = "dss-proxy-actions-optimized"; solcFlags = "--optimize"; };
-    dss-deploy-1_2 = deps'.dss-deploy-1_2           // { name = "dss-deploy-1_2";              solc = solc-versions.solc_0_5_12; };
-    dss-deploy = deps'.dss-deploy                   // { name = "dss-deploy";                  solc = solc-static-versions.solc_0_6_12; };
-    dss-deploy-optimized-runs-1 = deps'.dss-deploy  // { name = "dss-deploy-optimized-runs-1"; solcFlags = "--optimize --optimize-runs 1"; solc = solc-static-versions.solc_0_6_12; };
-    symbolic-voting = deps'.symbolic-voting         // { name = "symbolic-voting";             solc = solc-static-versions.solc_0_6_6; };
-    vote-delegate = deps'.vote-delegate             // { name = "vote-delegate-optimized";     solcFlags = "--optimize --optimize-runs 200"; solc = solc-static-versions.solc_0_6_12; };
-    dss-vest = deps'.dss-vest                       // { name = "dss-vest-optimized";          solcFlags = "--optimize --optimize-runs 200"; solc = solc-static-versions.solc_0_6_12; };
-    dss-chain-log = deps'.dss-chain-log             // { name = "dss-chain-log-optimized";     solcFlags = "--optimize --optimize-runs 200"; solc = solc-static-versions.solc_0_6_12; };
+    dss-deploy = deps'.dss-deploy                   // { name = "dss-deploy";                  solcFlags = "--optimize --optimize-runs 1"; };
+    dss-vote-delegate = deps'.dss-vote-delegate     // { name = "dss-vote-delegate-optimized"; solcFlags = "--optimize --optimize-runs 200"; };
+    dss-vest = deps'.dss-vest                       // { name = "dss-vest-optimized";          solcFlags = "--optimize --optimize-runs 200"; };
+    dss-chain-log = deps'.dss-chain-log             // { name = "dss-chain-log-optimized";     solcFlags = "--optimize --optimize-runs 200"; };
   });
 
 in makerScriptPackage {
---
+++ dss-init.sh
@@ -0,0 +1,26 @@
+alias mainnet-on='export ETH_RPC_URL=https://xsc-datarpc1.xt.com'
+alias testnet-on='export ETH_RPC_URL=https://testnet4.xtvip.top'
+
+export ETH_PASSWORD=/dev/null
+export ETH_GAS=7000000
+export ETH_KEYSTORE=./keystore
+export ETH_FROM=0x019afbcE1C05bE822c2Fd3D3d76eEaaf5ac90D0e
+
+export LIB_DIR=/home/xdao/apps/xss-deploy-scripts/lib
+export OUT_DIR=/home/xdao/apps/xss-deploy-scripts/out
+
+testnet-on
+
+export PATH=$PATH:/usr/bin
+
+loadAddresses() {
+    local keys
+    ADDRESSES_FILE="/home/xdao/apps/xss-deploy-scripts/out/addresses.json"
+    keys=$(jq -r "keys_unsorted[]" "$ADDRESSES_FILE")
+    for KEY in $keys; do
+        VALUE=$(jq -r ".$KEY" "$ADDRESSES_FILE")
+        export "$KEY"="$VALUE"
+    done
+}
+
+loadAddresses
